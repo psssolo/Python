66655e5880ded4f0ec17ea6b9d2ffbfe473d9536
psssolo <pesssolo@gmail.com>
2019-07-07 07:31:01 -0400

    Update 1_3.py

--- 1_3.py
+++ 1_3.py
@@ -4,4 +4,5 @@ This program gets a number and prints a pyramid of stars right aligned, with siz
 
 x = int(input("Please select a number:"))
 
-for i in range(1, x + 1): print("*" * i)
+for i in range(1, x + 1):
+  print("*" * i)
