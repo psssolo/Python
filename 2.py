41961f3cc745e561145884f53596512511dfb374
psssolo <pesssolo@gmail.com>
2019-07-07 17:42:47 -0400

    Update 2.py

--- 2.py
+++ 2.py
@@ -8,7 +8,7 @@ import re
 def is_winner(game_pattern):
     return (re.search(r'^(.{3})*(x{3}|o{3})', game_pattern) or
             re.search(r'^.{0,2}([xo])..\1..\1.?', game_pattern) or
-            re.search(r'([xo])...\1...\1|..([xo]).\1.\1..', game_pattern))
+            re.search(r'([xo])...\1...\1|..([xo]).\2.\2..', game_pattern))
 
 
 if is_winner(input("Please enter the game pattern")):
